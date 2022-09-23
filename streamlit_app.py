c5628f4834ce4ea8146985fc542bacb30d1ca7f9
weau2k <114212940+weau2k@users.noreply.github.com>
2022-09-23 10:47:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 import pandas 
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
                                 
 
 streamlit.title ('My Parents New Healthy Diner')
@@ -14,3 +13,6 @@ streamlit.text ('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text ('🥑 🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
