7a1aa6d22eb567dc6c96e70ce6009a70ec5c118b
weau2k <114212940+weau2k@users.noreply.github.com>
2022-09-29 08:30:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,7 @@ fruist_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.dataframe(fruist_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response) 
